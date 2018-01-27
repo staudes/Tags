7dcfbd48f0986dfc0e6770d193543399f323a234
Christoph Staudinger <chstaudi@gmail.com>
2018-01-27 23:01:16 +0100

    some progress

--- tags.py
+++ tags.py
@@ -8,13 +8,12 @@ def find_tags(start_dir):
     for p, d, files in os.walk(start_dir):
         for f in files:
             if f == '.tags':
-                yield p
+                yield p + '/.tags'
 
 def create_link(target, name):
-
     os.symlink(target, name)
 
-def process_line(line):
+def parse_line(line):
     try:
         tag, name = line.split(',')
         if not name.strip():
@@ -22,16 +21,32 @@ def process_line(line):
     except ValueError:
         tag = line
         name = None
-    if os.path.exists(tag):
-        if not os.path.isdir(tag):
-            raise FileExistsError(tag + ' exists and is no directory!')
-    else:
-        os.path.mkdir(tag)
-    os.chdir(tag)
-    create_link(target, name)
-    os.chdir('..')
+    return tag.split('/'), name
 
-def process_file(tags_file):
+def create_dirs(base_dir, dirs: list):
+    base_path = os.path.abspath(base_dir)
+    os.chdir(base_path)
+    for d in dirs:
+        if os.path.exists(d):
+            if not os.path.isdir(d) or os.path.islink(d):
+                raise ValueError('Cannot create directory "{}". Path is blocked'.format(d))
+        else:
+            os.mkdir(d)
+            os.chdir(d)
+    os.chdir(base_path)
+
+def process_file(tags_file, base_dir):
     with open(tags_file, 'r') as f:
         for l in f.readlines():
-            process_line(l)
+            dirs, name = parse_line(l)
+            if name is None:
+                name = tags_file.rsplit('/.', 1)[0].rsplit('/', 1)[1]
+            create_dirs(base_dir, dirs)
+            create_link(tags_file.rsplit('/', 1)[0], name)
+
+def process_file(conf):
+    with open(conf, 'r') as f:
+        path, tag_dir = f.read().strip().split('\n')
+        for t in find_tags(path):
+            process_file(t)
+
